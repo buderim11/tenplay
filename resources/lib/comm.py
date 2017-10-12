77c6c2675935c737428169d0eb439f08a3042aee
Glenn Guy <glennguy83@gmail.com>
2017-10-12 17:28:26 +1100

    remove unicode tags, update logic

--- resources/lib/comm.py
+++ resources/lib/comm.py
@@ -24,46 +24,42 @@ def fetch_url(url):
 def get_shows(params):
     data = json.loads(fetch_url(config.SHOWLIST_URL))
     listing = []
-    for show in data[u'Browse TV'][u'Shows']:
-        x = len(show[u'Seasons'])
-        multi_season = x > 1
-        while x >= 1:
+    for show in data['Browse TV']['Shows']:
+        x = len(show['Seasons'])
+        multi_season = len(show['Seasons']) > 1
+        for x, season in reversed(list(enumerate(show['Seasons']))):
             s = classes.series()
-            s.query = show[u'query']
-            s.thumb = show[u'videoStillURL']
-            s.fanart = show[u'bannerURL']
-            s.season = show[u'Seasons'][x-1]
-            s.genre = show[u'genre']
+            s.query = show['query']
+            s.thumb = show['videoStillURL']
+            s.fanart = show['bannerURL']
+            s.season = show['Seasons'][x-1]
+            s.genre = show['genre']
             if multi_season:
-                s.title = '{0} Season {1}'.format(show[u'title'], s.season)
+                s.title = '{0} Season {1}'.format(show['title'], s.season)
             else:
-                s.title = show[u'title']
+                s.title = show['title']
             listing.append(s)
-            x -= 1
     return listing
 
 
 def get_episodes(params):
-    if 'page' not in params:
-        page = 0
-    else:
-        page = params['page']
+    page = params.get('page', 0)
     url = config.EPISODEQUERY_URL.format(
         urllib.unquote(params['query']), page, params['season'])
     if params['category'] == 'News' or params['category'] == 'Sport':
         url = url.replace('&all=video_type_long_form:Full+Episodes', '')
     data = json.loads(fetch_url(url))
     listing = []
-    for episode in data[u'items']:
+    for episode in data['items']:
         e = classes.episode()
-        e.thumb = episode[u'videoStillURL']
-        e.fanart = urllib.unquote(params['fanart'])
-        e.title = episode[u'customFields'][u'clip_title']
-        e.desc = episode[u'shortDescription']
-        e.duration = episode[u'length']/1000
-        e.airdate = episode[u'customFields'][u'start_date_act']
+        e.thumb = episode['videoStillURL']
+        e.fanart = urllib.unquote(params.get('fanart', ''))
+        e.title = episode['customFields']['clip_title']
+        e.desc = episode['shortDescription']
+        e.duration = episode['length']/1000
+        e.airdate = episode['customFields']['start_date_act']
         e.page = int(page)
-        e.id = episode[u'id']
+        e.id = episode['id']
         e.total_episodes = int(data['total_count'])
         if e.total_episodes > 30:
             e.query = urllib.quote(params['query'])
@@ -91,6 +87,6 @@ def get_featured():
 def get_genres():
     data = json.loads(fetch_url(config.SHOWLIST_URL))
     listing = []
-    for genre in data[u'Browse TV'][u'Genres']:
+    for genre in data['Browse TV']['Genres']:
         listing.append(genre)
     return listing
